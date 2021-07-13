65abcbafb3433b9fb69d684b0fc9e970ee97dc62
zmaljloob <86270190+zmaljloob@users.noreply.github.com>
2021-07-13 19:11:46 +0530

    Enviornment Setting Done

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tron Fittness</title>
+    <?php include_once "common/bootstrap.html"?>
+</head>
+<body>
+    <?php include_once "common/header.html"?>
+    <?php include_once "common/slider.html"?>
+    <?php include_once "common/footer.html"?>
+</body>
+</html>
