cac6afa9e5a0d2bbc47f0c1a7d1eaa5193821b05
zmaljloob <86270190+zmaljloob@users.noreply.github.com>
2021-07-20 22:00:43 +0530

    Contact

--- contact.php
+++ contact.php
@@ -6,9 +6,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tron Fittness</title>
     <?php include_once "common/bootstrap.html"?>
+    <style>
+        body{
+            background: url("img/b3.jpg");
+            background-attachment: fixed;
+        }
+        div.col-md-4{
+            height: 400px !important;
+        }
+        .trans-gray{
+            background-color: rgba(255,255,255,0.30);
+        }
+    </style>
 </head>
 <body>
     <?php include_once "common/header.html"?>
+    <div class="container p-5">
+        <div class="row p-0 text-light">
+            <div class="col-md-4 trans-gray">Contact</div>
+            <div class="col-md-8 trans-gray">Map</div>
+        </div>
+    </div>
     <?php include_once "common/footer.html"?>
     <script>
         set_page_active("cont");
